53a6a8ce494d8d7076ef49e7d79797c80149fe6d
SamamaKarim092 <samamakarim90@gmail.com>
2025-08-17 16:28:50 +0500

    created the card

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -2,39 +2,49 @@ import React from "react";
 
 const Cards = () => {
   return (
-    <div>
-      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <div className="flex flex-wrap justify-center gap-6 mt-16">
+
+      {/* Card 1 */}
+      <div className="max-w-xl p-10 bg-white border border-gray-200 rounded-lg shadow-sm h-104">
+        <p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
+            Types Of Business And Personal Accounts
+          </h5>
+        </p>
+        <p className="mb-3 font-normal text-gray-700 text-center">
+          You Can Easily Open Any Type Of Account, Including Personal And
+          Butiness, And Use All The Services You Noed Eatily And Online. We
+          Wil Update And Add Global Services For You.
+        </p>
+      </div>
+
+      {/* Card 2 */}
+      <div className="max-w-xl p-10 bg-white border border-gray-200 rounded-lg shadow-sm h-104">
+        <p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
+            Opening Of Essential Bank Account
+          </h5>
+        </p>
+        <p className="mb-3 font-normal text-gray-700 text-center">
+          It Is Not impossible To Open An Account in An Online And Simple
+          Way With Just Two Clicks. You Can Now Save Your Time And
+          Creato A Fast And Safe flank Account For Yourself Or Your Loved Ones.
+        </p>
+      </div>
+
+      {/* Card 3 */}
+      <div className="max-w-xl p-10 bg-white border border-gray-200 rounded-lg shadow-sm h-104">
+        <p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
             Noteworthy technology acquisitions 2021
           </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        </p>
+        <p className="mb-3 font-normal text-gray-700 text-center">
           Here are the biggest enterprise technology acquisitions of 2021 so
           far, in reverse chronological order.
         </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
       </div>
+
     </div>
   );
 };
