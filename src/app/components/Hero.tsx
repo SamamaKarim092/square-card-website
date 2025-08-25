a0a91ef1413c43501181bec2a87e59073a07baf0
SamamaKarim092 <samamakarim90@gmail.com>
2025-08-25 18:47:24 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,15 +7,18 @@ const Hero = () => {
     <section className="bg-#eeebf0">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl">
-            A Modern Bank Card For A Modern World
+          <h1 className="max-w-2xl mb-12 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl">
+            A Modern Bank Card <br /> For A Modern World
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-            This Modern Bank Card Embraces The Era Of Contactless Payments, Enabling
-            Swift And Effortless Transactions With Just A Tap Or Wave. No More Fumbling For
-            Cash Or Struggling With Outdated Payment Methods.
+          <p className="max-w-2xl mb-6 font-light text-black font-semibold lg:mb-8 md:text-lg lg:text-xl">
+            This Modern Bank Card Embraces The Era Of Contactless Payments,
+            Enabling Swift And Effortless Transactions With Just A Tap Or Wave.
+            No More Fumbling For Cash Or Struggling With Outdated Payment
+            Methods.
           </p>
-          <Button>Explore More</Button>
+          <Button className="mt-6 bg-[#d0f600] text-black border-2 border-black hover:bg-[#b9dd00]">
+            Explore More
+          </Button>
         </div>
 
         {/* Optimized Image */}
